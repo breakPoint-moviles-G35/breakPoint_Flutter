1abe4c55896439cef8bf0fa04dbff10ec5bf32ab
Alejandro Lancheros <a.lancherosd@uniandes.edu.co>
2025-09-20 02:26:26 -0500

    add: reservation and dialgo reservation screen v1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'routes/app_router';
 import 'presentation/login/login_screen';
 import 'presentation/explore/explore_screen';
+import 'presentation/details/space_detail_screen.dart';
 import 'presentation/filters/date_filter_screen.dart'; 
 void main() {
   runApp(const MyApp());
@@ -23,6 +24,12 @@ class MyApp extends StatelessWidget {
         AppRouter.login: (context) => const LoginScreen(),
         AppRouter.explore: (context) => const ExploreScreen(),
         AppRouter.filters: (context) => const DateFilterScreen(),
+        AppRouter.spaceDetail: (context) => SpaceDetailScreen(
+          title: 'Sample Space',
+          subtitle: 'A nice place to stay',
+          rating: 4.5,
+          price: 12000.0, 
+        ),
       },
     );
   }
