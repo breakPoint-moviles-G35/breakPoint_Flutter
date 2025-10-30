7ea1c4fb6d856e5f371e3fc9e30d52679a2fcdcd
Martinez Oliveros Jose David <jd.martinezo1@sis.virtual.uniandes.edu.co>
2025-10-30 11:18:44 -0500

    fix: login

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ Future<void> main() async {
   //  SIMULADOR : 'http://10.0.2.2:3000' 
   // Configuración de red y repos
   final dioClient = DioClient(
-    'http://157.253.225.104:3000',
+    'http://10.0.2.2:3000',
     tokenProvider: () => authRepoRef?.token,
   );
   final api = SpaceApi(dioClient.dio);
